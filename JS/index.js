bb2a194ebd9383586a7280417009889f5e09ed95
Onohana Jino <serohijoscjhay78@gmail.com>
2023-12-12 15:29:48 +0800

    none

--- JS/index.js
+++
@@ -1,7 +0,0 @@
-function mover() {
-  var listedButton = document.querySelector('.listed-button');
-  var logoMiddle = document.querySelector('.logo-middle');
-
-  listedButton.classList.toggle('visible');
-  logoMiddle.classList.toggle('clicked');
-  }
